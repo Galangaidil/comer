a02880360b9f30c82fe5fd108007c678ac82639c
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2022-06-12 18:29:53 +0700

    add profile

--- profile.php
+++ profile.php
@@ -1,15 +1,20 @@
+<?php
+
+require_once('components/nav.php');
+
+$nav = new Nav();
+
+?>
+
 <!doctype html>
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Profile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
     <style>
         .gradient-custom {
             /* fallback for old browsers */
@@ -19,54 +24,15 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">Janapedia</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <div class="navbar-nav mx-auto mb-2 mb-lg-0">
-                    <form class="d-flex" role="search">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Cari produk" aria-label="Recipients username" aria-describedby="basic-addon2">
-                            <span class="input-group-text" id="basic-addon2"><i class="bx bx-search"></i></span>
-                        </div>
-                    </form>
-                </div>
-                <div class="d-flex align-items-center gap-2">
-                    <button class="btn">
-                        <i class="bx bx-cart"></i>
-                    </button>
+    <?= $nav->index() ?>
 
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="bx bxs-user"></i>
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-end">
-                            <li>
-                                <h6 class="dropdown-header">Pengaturan</h6>
-                            </li>
-                            <li><button class="dropdown-item" type="button">John Doe</button></li>
-                            <li><button class="dropdown-item" type="button">Settings</button></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><button class="dropdown-item" type="button">Log out</button></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </nav>
-    <section class="vh-100" style="background-color: #f4f5f7;">
+    <div class="container-fluid" style="background-color: f4f5f7;">
         <div class="container py-5 h-100">
-            <!-- d-flex justify-content-center align-items-center h-100 -->
             <div class="row ">
                 <div class="col col-lg-12 mb-4 mb-lg-0">
                     <div class="card mb-3" style="border-radius: .5rem;">
                         <div class="row g-0">
-                            <div class="col-md-4 gradient-custom text-center text-white" style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem;">
+                            <div class="col-md-4 gradient-custom text-center text-white" style="border-radius: .5rem; ">
                                 <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp" alt="Avatar" class="img-fluid my-5" style="width: 200px;" />
                                 <h4>Samuel</h4>
                                 <p>Member</p>
@@ -74,7 +40,7 @@
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body p-4">
-                                    <h6>Informasi Akun</h6>
+                                    <h5>Informasi Akun</h5>
                                     <hr class="mt-0 mb-4">
                                     <div class="row pt-1">
                                         <div class="col-6 mb-3">
@@ -83,10 +49,10 @@
                                         </div>
                                         <div class="col-6 mb-3">
                                             <h6>Password</h6>
-                                            <p class="text-muted">*******</p>
+                                            <p class="text-muted">***</p>
                                         </div>
                                     </div>
-                                    <h6>Alamat</h6>
+                                    <h5>Alamat</h5>
                                     <hr class="mt-0 mb-4">
                                     <div class="row pt-1">
                                         <div class="col-6 mb-3">
@@ -103,15 +69,6 @@
                                             <h6>Alamat lengkap</h6>
                                             <p class="text-muted">Jl. Siliwangi Jl. Ring Road Utara, Jombor Lor, Sendangadi, Kec. Mlati,</p>
                                         </div>
-                                        <!-- <div class="col-6 mb-3">
-                                            <h6>Kota</h6>
-                                            <p class="text-muted">Kabupaten Sleman</p>
-                                        </div> -->
-                                    </div>
-                                    <div class="d-flex justify-content-start">
-                                        <a href="#!"><i class="fab fa-facebook-f fa-lg me-3"></i></a>
-                                        <a href="#!"><i class="fab fa-twitter fa-lg me-3"></i></a>
-                                        <a href="#!"><i class="fab fa-instagram fa-lg"></i></a>
                                     </div>
                                 </div>
                             </div>
@@ -120,20 +77,11 @@
                 </div>
             </div>
         </div>
-    </section>
-    <!-- Optional JavaScript; choose one of the two! -->
+    </div>
 
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+    <?= $nav->footer() ?>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
-    -->
-    <script src="https://unpkg.com/boxicons@2.1.2/dist/boxicons.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
 
 </html>
