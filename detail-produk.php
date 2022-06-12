8bd5386961fcd5c2b7a1bc1da6d9be6f7b968515
R. Adi Permana <radipermana29@gmail.com>
2022-06-12 16:34:18 +0700

    update detail produk

--- detail-produk.php
+++ detail-produk.php
@@ -1,3 +1,10 @@
+<?php
+
+require_once 'components/nav.php';
+
+$nav = new Nav();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,45 +19,7 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
-
+    <?= $nav->index() ?>
     <div class="container">
         <nav class="my-4" style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
             <ol class="breadcrumb">
@@ -60,8 +29,8 @@
             </ol>
         </nav>
         <div id="produk" class="d-flex">
-            <div class="row">
-                <div class="col-4">
+            <div class="row flex-column flex-md-row">
+                <div class="col-3">
                     <img class="img-fluid" src="https://static.mercdn.net/item/detail/orig/photos/m13451739216_1.jpg?1643179806" />
                 </div>
                 <div class="col-6">
@@ -79,17 +48,114 @@
                         <div>
                             <h4 class="text-warning">Rp. 430.000</h4>
                         </div>
-                        
                     </div>
-                    <hr>
-                    <div class="deskripsi">
+                    <div class="deskripsi my-4">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat odio, voluptas, fuga omnis quo vel dignissimos enim totam dolore ullam a. Odio, aut sapiente voluptatem aliquam maxime iure incidunt libero. Corporis nostrum iste a odit maxime, fugiat architecto. Mollitia repudiandae tenetur, temporibus accusantium voluptas ab, omnis error, laborum amet nam quas pariatur modi nulla rerum. Possimus, sint deleniti temporibus quia saepe officia quasi debitis libero exercitationem, quae dignissimos facilis, recusandae minus eum placeat odio? Quae nisi dolor, soluta cumque corporis harum laborum nihil dolores nobis illum delectus quo itaque quam veritatis deleniti voluptates, quas distinctio consectetur vitae quisquam beatae excepturi!
-                    
-                    <hr>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card" style="width: 15rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Beli</h5>
+                            <p class="card-text"></p>
 
+                            <input type="number" class="form-control" placeholder="Masukkan jumlah" aria-label="Jumlah" aria-describedby="button-addon2" value="1">
+                            <button class="btn btn-primary my-2 form-control" type="submit"><i class='bx bx-cart-add me-2 font-lg'></i>Keranjang</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+    
+        <div class="ulasan mt-4">
+            <div class="row d-flex">
+                <div class="col-md-9">
+                    <div class="headings d-flex justify-content-between align-items-center mb-3">
+                        <h5>Ulasan Produk</h5>
+                    </div>
+                    <div class="card p-3 my-2">
+                        <div class="d-flex flex-row">
+                            <div class="user d-flex flex-row align-items-center">
+                                <img src="https://i.imgur.com/hczKIze.jpg" width="30" class="user-img rounded-circle mr-2">
+                            </div>
+                            <span class="mx-2"><small class="font-weight-bold text-primary">james_olesenn</small></span>
+                            <span class="ms-auto"><small>2 hari yang lalu</small></span>
+                        </div>
+                        <div class="d-flex py-2">
+                            Produk sangat bagus. Kualitas mantap dan original.
+                        </div>
+                        <div class="bintang">
+                        <i class='bx bxs-star text-warning'></i>
+                        <i class='bx bxs-star text-warning'></i>
+                        <i class='bx bxs-star text-warning'></i>
+                        </div>
+                    </div>
+                    <div class="card p-3 my-2">
+                        <div class="d-flex flex-row">
+                            <div class="user d-flex flex-row align-items-center">
+                                <img src="https://i.imgur.com/hczKIze.jpg" width="30" class="user-img rounded-circle mr-2">
+                            </div>
+                            <span class="mx-2"><small class="font-weight-bold text-primary">james_olesenn</small></span>
+                            <span class="ms-auto"><small>2 hari yang lalu</small></span>
+                        </div>
+                        <div class="d-flex py-2">
+                            Produk sangat bagus. Kualitas mantap dan original.
+                        </div>
+                        <div class="bintang">
+                        <i class='bx bxs-star text-warning'></i>
+                        <i class='bx bxs-star text-warning'></i>
+                        <i class='bx bxs-star text-warning'></i>
+                        </div>
+                    </div>
+
+                    <div class="card p-3 my-2">
+                        <div class="d-flex flex-row">
+                            <div class="user d-flex flex-row align-items-center">
+                                <img src="https://i.imgur.com/hczKIze.jpg" width="30" class="user-img rounded-circle mr-2">
+                            </div>
+                            <span class="mx-2"><small class="font-weight-bold text-primary">james_olesenn</small></span>
+                            <span class="ms-auto"><small>2 hari yang lalu</small></span>
+                        </div>
+                        <div class="d-flex py-2">
+                            Produk sangat bagus. Kualitas mantap dan original.
+                        </div>
+                        <div class="bintang">
+                        <i class='bx bxs-star text-warning'></i>
+                        <i class='bx bxs-star text-warning'></i>
+                        <i class='bx bxs-star text-warning'></i>
+                        <i class='bx bxs-star text-warning'></i>
+                        </div>
+                    </div>
+
+                    <div class="card p-3 my-2">
+                        <div class="d-flex flex-row">
+                            <div class="user d-flex flex-row align-items-center">
+                                <img src="https://i.imgur.com/hczKIze.jpg" width="30" class="user-img rounded-circle mr-2">
+                            </div>
+                            <span class="mx-2"><small class="font-weight-bold text-primary">james_olesenn</small></span>
+                            <span class="ms-auto"><small>2 hari yang lalu</small></span>
+                        </div>
+                        <div class="d-flex py-2">
+                            Produk sangat bagus. Kualitas mantap dan original.
+                        </div>
+                        <div class="bintang">
+                        <i class='bx bxs-star text-warning'></i>
+                        <i class='bx bxs-star text-warning'></i>
+                        <i class='bx bxs-star text-warning'></i>
+                        </div>
+                    </div>
+
+                    
+                </div>
+
+            </div>
+        </div>
+
+        <div class="rekomendasi">
+
+        
+        </div>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
